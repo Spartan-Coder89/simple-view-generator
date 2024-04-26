492e62f278a352e3a0535e5a5f6a2c6df69d1fed
Simon Jiloma <sjiloma1389@gmail.com>
2024-04-26 23:28:39 +0800

    To be replaced

--- README.md
+++
@@ -1,2 +0,0 @@
-# simple-view-generator
-Generates a view as configured in a json file
--- templates/front.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-get_partial_template('sidebar.php');
-echo '<br>';
-get_partial_template('header.php');
-echo '<br>';
-echo 'This is frontpage template';
-echo '<br>';
